9b20480e88bbc98a4e8b6ea69fc4abcd9e8a0189
NarumiMashiro <narunarumashiro96@gmail.com>
2024-04-13 12:36:56 +0900

    revert index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,11 @@
 import { useTranslation } from 'next-i18next'
-import { useState } from 'react'
-
-import { useTheme } from '@mui/material'
-import { SampleApi } from '@/recoil/services/sampleApi'
 
 // MyComponents
 import Meta from "@/components/meta"
 
-import { Accordion } from '@/stories/Accordion/Accordion'
-
 const Home = () => {
 
   const { t } = useTranslation()
-  const colorTheme = useTheme().palette.mode
-
-  const [open, setOpen] = useState(false)
-  const temp = {
-    width: '300px'
-  }
-
-  const onTemp = () => {
-    setOpen(false)
-    sampleApi(false)
-  }
 
   const { sampleApiFetchState, sampleApi, resetSampleApi } = SampleApi()
 
@@ -32,14 +15,7 @@ const Home = () => {
       <Meta pageTitle={t('STRID_cmn_pagetitle').replace('{var}', 'Project Title')}/>
       <div>
         <span>Hello, NextJs!!!</span>
-        <button onClick={() => sampleApi(true)}>temp</button>
       </div>
-      <div style={{width: 500, marginTop: 100}}>
-      <Accordion
-        summary={'temp'}
-      />
-      </div>
-      
     </>
   )
 }
