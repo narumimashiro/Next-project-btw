6aa21be1dd5f8307724814d39ccf65914638e329
NarumiMashiro <narunarumashiro96@gmail.com>
2024-04-13 12:40:39 +0900

    revert index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,12 @@ const Home = () => {
 
   const { t } = useTranslation()
 
-  const { sampleApiFetchState, sampleApi, resetSampleApi } = SampleApi()
-
   return (
     <>
     {/* TODO change meta title to suit your project */}
       <Meta pageTitle={t('STRID_cmn_pagetitle').replace('{var}', 'Project Title')}/>
       <div>
-        <span>Hello, NextJs!!!</span>
+        <h1>Hello, NextJs!!!</h1>
       </div>
     </>
   )
