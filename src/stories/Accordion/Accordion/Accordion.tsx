639bc51e1862b8a38f4976c87307bc8edb6c4f91
NarumiMashiro <narunarumashiro96@gmail.com>
2024-06-20 20:54:30 +0900

    delete unused var

--- src/stories/Accordion/Accordion/Accordion.tsx
+++ src/stories/Accordion/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, CSSProperties } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import { useTheme } from '@mui/material'
 import ArrowDownLight from '@/img/light/arrow_down_light.svg'
