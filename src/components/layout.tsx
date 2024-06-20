69637d4e21f93411ebdda39fc45b5cb861589c7e
NarumiMashiro <narunarumashiro96@gmail.com>
2024-06-20 20:53:06 +0900

    delete unused var

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Layout.module.scss'
-import { useCustomContext } from './customProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
