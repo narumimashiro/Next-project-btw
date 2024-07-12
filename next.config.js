753510a0264443c70b6c380c7e75d3f39e8f53d5
NarumiMashiro <narunarumashiro96@gmail.com>
2024-07-12 21:18:56 +0900

    revert edit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config')
 
-if (typeof performance === 'undefined') {
-  global.performance = require('perf_hooks').performance
-}
-
 const nextConfig = {
   i18n,
   compiler: {
