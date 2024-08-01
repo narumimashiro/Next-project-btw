9b4994001740853f68ba3e7dde8e83b3366aa49e
NarumiMashiro <narunarumashiro96@gmail.com>
2024-08-02 00:42:48 +0900

    annotation text created

--- src/components/atom/componentsTemplate.tsx
+++ src/components/atom/componentsTemplate.tsx
@@ -1,4 +1,7 @@
 import { useCustomContext } from '@/components/customProvider'
+import { useTheme } from '@mui/material'
+
+import styles from '@/styles/atom/ComponentsTemplate.module.scss'
 
 type TextProps = {
   children: React.ReactNode
@@ -19,4 +22,13 @@ export const BodyText = ({ children, className = '' }: TextProps) => {
   const { isPortrait } = useCustomContext()
 
   return <p className={`${isPortrait ? 'text-sm' : 'text-base'} ${className}`}>{children}</p>
+}
+
+export const AnnotationText = ({ children, className = '' }: TextProps) => {
+  const theme = useTheme()
+  const colorTheme = theme.palette.mode
+
+  return (
+    <p className={`text-xs ${styles[`annotation-${colorTheme}`]} ${className}`}>{children}</p>
+  )
 }
