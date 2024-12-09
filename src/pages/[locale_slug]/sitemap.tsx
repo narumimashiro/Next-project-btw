778b808dfe4a170ee07043f1ba79d5e361450116
NarumiMashiro <narunarumashiro96@gmail.com>
2024-12-09 21:58:15 +0900

    complete sitemap

--- src/pages/[locale_slug]/sitemap.tsx
+++ src/pages/[locale_slug]/sitemap.tsx
@@ -2,6 +2,13 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 
 import Meta from '@/components/meta'
+import { useCustomContext } from '@/components/customProvider'
+import { HeadlineText, SubTitleText } from '@/components/atom/componentsTemplate'
+import { useLocaleSlug } from '@/hooks/useLocaleSlug'
+import { useManageMenuHidden } from '@/recoil/manageMenu'
+import { TextLink } from '@/stories/Link/TextLink'
+
+import styles from '@/styles/Sitemap.module.scss'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const { language }: { language: string[] } = require('@/locales/config')
@@ -25,13 +32,74 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   }
 }
 
+type LinkGroupingProps = {
+  groupingTitle: string
+  linkList: {
+    text: string
+    src: string
+  }[]
+}
+const LinkGrouping = ({ groupingTitle, linkList }: LinkGroupingProps) => {
+  const { isPortrait } = useCustomContext()
+
+  return (
+    <div className={styles.linkGroup}>
+      <SubTitleText className="mb-16">{groupingTitle}</SubTitleText>
+      <div className={styles[`grid-${isPortrait ? 'portrait' : 'landscape'}`]}>
+        {linkList.map((link) => (
+          <TextLink key={link.text} href={link.src} text={link.text} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
 const Sitemap = () => {
   const { t } = useTranslation()
+  const { isPortrait } = useCustomContext()
+  const locale = useLocaleSlug()
+  useManageMenuHidden()
+
+  /**
+   * Sitemap contents list
+   */
+  const topContents = [{ text: t('STRID_sitemap_top'), src: `/${locale}` }]
+  const hobbyContents = [
+    {
+      text: t('STRID_meta_prsk_music'),
+      src: `/${locale}/prsk-music`
+    },
+    {
+      text: t('STRID_meta_mygo_quiz'),
+      src: `/${locale}/mygo-quiz`
+    },
+    {
+      text: t('STRID_meta_alya'),
+      src: `/${locale}/alya-russian`
+    },
+    {
+      text: t('STRID_meta_otaku_webcamera'),
+      src: `/${locale}/web-camera/otaku-camera`
+    }
+  ]
+  const otherContents = [
+    {
+      text: t('STRID_meta_about'),
+      src: `/${locale}/about`
+    }
+  ]
 
   return (
     <>
       <Meta pageTitle={t('STRID_cmn_pagetitle').replace('{var}', t('STRID_menu_sitemap'))} />
-      <p className="absolute-center text-2xl-bold">{t('STRID_cmn_unsupport')}</p>
+      <div className={`${styles['page-container']} ${isPortrait ? styles.portrait : ''}`}>
+        <HeadlineText className="mb-24">{t('STRID_menu_sitemap')}</HeadlineText>
+        <div className={styles['contents-container']}>
+          <LinkGrouping groupingTitle={t('STRID_sitemap_top')} linkList={topContents} />
+          <LinkGrouping groupingTitle={t('STRID_sitemap_hobby')} linkList={hobbyContents} />
+          <LinkGrouping groupingTitle={t('STRID_sitemap_other')} linkList={otherContents} />
+        </div>
+      </div>
     </>
   )
 }
