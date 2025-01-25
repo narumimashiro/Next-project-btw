7a5b391fc4d052e677ef4c0a8f642da12b942a2f
HiRO / Narumi <narunarumashiro96@gmail.com>
2025-01-25 21:29:20 +0900

    fix locale bug priority setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,18 +7,28 @@ import '@/locales/config'
 
 import { CustomProvider } from '@/components/customProvider'
 import { LayoutWithMenu } from '@/components/layout'
-import { useLocaleSlug, I18NEXT_LOCALE } from '@/hooks/useLocaleSlug'
+import {
+  useLocaleSlug,
+  I18NEXT_LOCALE,
+  useUrlLocaleSlug,
+  useRouterLocale
+} from '@/hooks/useLocaleSlug'
 
 import '@/styles/globals.scss'
-
 const App = ({ Component, pageProps }: AppProps) => {
-  const locale = useLocaleSlug()
   const { i18n } = useTranslation()
+  const locale = useLocaleSlug()
+  const urlLanguage = useUrlLocaleSlug()
+  const { routerNewUrl } = useRouterLocale()
 
   useEffect(() => {
     i18n.changeLanguage(locale)
     localStorage.setItem(I18NEXT_LOCALE, locale)
-  }, [i18n, locale])
+    if (urlLanguage !== locale) {
+      routerNewUrl(locale)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [i18n, locale, urlLanguage])
 
   return (
     <RecoilRoot>
